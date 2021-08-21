f1f6d7aacf403c38ae2fc575d643bc8b486ffc7a
Sergey Dedikov <serded59@yandex.ru>
2021-08-21 15:59:01 +0500

    fix: merge scripts's files

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,21 @@
+/* Open-Close popup */
+
+let popup = document.querySelector(".popup");
+let editButton = document.querySelector(".profile__button_edit");
+let closeButton = document.querySelector(".popup__button_close");
+
+function popupOpened() {
+  popup.classList.add("popup_opened");
+}
+editButton.addEventListener("click", popupOpened);
+
+function popupClosed() {
+  popup.classList.remove("popup_opened");
+}
+closeButton.addEventListener("click", popupClosed);
+
+/* Edit profile + Submit form*/
+
 let formElement = document.querySelector(".popup__container");
 let nameInput = formElement.querySelector(".popup__item_name");
 let jobInput = formElement.querySelector(".popup__item_job");
