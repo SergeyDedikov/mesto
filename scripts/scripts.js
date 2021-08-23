59734eb5aef8b868492b1c9a95b20d3478b36b9d
Sergey Dedikov <serded59@yandex.ru>
2021-08-23 20:17:26 +0500

    fix: scripts

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,37 +1,29 @@
-/* Open-Close popup */
-
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__button_type_edit");
 let closeButton = document.querySelector(".popup__button_type_close");
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__item_value_name");
+let jobInput = formElement.querySelector(".popup__item_value_job");
+let nameProfile = document.querySelector(".profile__name");
+let jobProfile = document.querySelector(".profile__job");
 
 function popupOpened() {
   popup.classList.add("popup_opened");
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
-editButton.addEventListener("click", popupOpened);
 
 function popupClosed() {
   popup.classList.remove("popup_opened");
 }
-closeButton.addEventListener("click", popupClosed);
-
-/* Edit profile + Submit form*/
-
-let formElement = document.querySelector(".popup__container");
-let nameInput = formElement.querySelector(".popup__item_value_name");
-let jobInput = formElement.querySelector(".popup__item_value_job");
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  nameInput.getAttribute("value");
-  jobInput.getAttribute("value");
-
-  let nameProfile = document.querySelector(".profile__name");
-  let jobProfile = document.querySelector(".profile__job");
-
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-
   popupClosed();
 }
+
+editButton.addEventListener("click", popupOpened);
+closeButton.addEventListener("click", popupClosed);
 formElement.addEventListener("submit", formSubmitHandler);
