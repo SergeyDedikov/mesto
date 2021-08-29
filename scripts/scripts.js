7bdd1ddf3ba8ae2cbcc5eff0df961a5231c9e1ea
Sergey Dedikov <serded59@yandex.ru>
2021-08-29 19:39:12 +0500

    feat: popup add items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,29 +1,48 @@
-const popup = document.querySelector(".popup");
+const popupEditProfile = document.querySelector(".popup_edit-profile");
+const popupAddPlace = document.querySelector(".popup_add-place");
 const buttonEditProfile = document.querySelector(".profile__button_type_edit");
-const buttonClose = document.querySelector(".popup__button_type_close");
+const buttonAddPlace = document.querySelector(".profile__button_type_add");
 const formElement = document.querySelector(".popup__container");
+const buttonCloseEditProfile = document.querySelector(
+  ".popup__button_type_close-edit-profile"
+);
+const buttonCloseAddPlace = document.querySelector(
+  ".popup__button_type_close-add-place"
+);
+
 const nameInput = formElement.querySelector(".popup__item_value_name");
 const jobInput = formElement.querySelector(".popup__item_value_job");
 const nameProfile = document.querySelector(".profile__name");
 const jobProfile = document.querySelector(".profile__job");
 
-function openPopup() {
-  popup.classList.add("popup_opened");
+function openPopupEditProfile() {
+  popupEditProfile.classList.add("popup_opened");
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove("popup_opened");
+function closePopupEditProfile() {
+  popupEditProfile.classList.remove("popup_opened");
 }
 
-function submitFormHandler(evt) {
+function submitFormProfile(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  closePopup();
+  closePopupEditProfile();
 }
 
-buttonEditProfile.addEventListener("click", openPopup);
-buttonClose.addEventListener("click", closePopup);
-formElement.addEventListener("submit", submitFormHandler);
+function openPopupAddPlace() {
+  popupAddPlace.classList.add("popup_opened");
+}
+
+function closePopupAddPlace() {
+  popupAddPlace.classList.remove("popup_opened");
+}
+
+buttonEditProfile.addEventListener("click", openPopupEditProfile);
+buttonCloseEditProfile.addEventListener("click", closePopupEditProfile);
+formElement.addEventListener("submit", submitFormProfile);
+
+buttonAddPlace.addEventListener("click", openPopupAddPlace);
+buttonCloseAddPlace.addEventListener("click", closePopupAddPlace);
