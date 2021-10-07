0ad10757afa70e4526964a2bbfcf4f795581692e
Sergey Dedikov <serded59@yandex.ru>
2021-10-07 15:51:43 +0500

    feat: create class Section

--- components/Section.js
+++ components/Section.js
@@ -1,5 +1,16 @@
-export class Section {
-  constructor() {
-    
+export default class Section {
+  constructor({ items, renderer }, selectorContainer) {
+    this._renderedItems = items;
+    this._renderer = renderer;
+
+    this._container = selectorContainer;
+  }
+
+  renderItems() {
+    this._renderedItems.forEach(item => this._renderer(item));
+  }
+
+  addItem(element) {
+    this._container.append(element);
   }
 }
