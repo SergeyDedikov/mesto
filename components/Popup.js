ea8463fe487bac6e2418b95c24024fdeef09b098
Sergey Dedikov <serded59@yandex.ru>
2021-10-07 19:03:58 +0500

    feat: create class Popup

--- components/Popup.js
+++ components/Popup.js
@@ -1,5 +1,40 @@
 export default class Popup {
-  constructor() {
-    
+  constructor(popupSelector) {
+    this._popup = popupSelector;
+    this._buttonClose = this._popup.querySelector('.popup__close');
+  }
+
+  open() {
+    this._popup.classList.add("popup_opened");
+    this.setEventListeners();
+  }
+
+  close() {
+    this._popup.classList.remove("popup_opened");
+    this.removeEventListeners();
+  }
+
+  _handleEscClose = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  _closeOnClick = (evt) => {
+    if (evt.target.classList.contains("popup")) {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    document.addEventListener("keydown", this._handleEscClose);
+    this._buttonClose.addEventListener("click", this.close());
+    this._popup.addEventListener("click", this._closeOnClick);
+  }
+
+  removeEventListeners() {
+    document.removeEventListener("keydown", this._handleEscClose);
+    this._buttonClose.removeEventListener("click", this.close());
+    this._popup.removeEventListener("click", this._closeOnClick);
   }
 }
