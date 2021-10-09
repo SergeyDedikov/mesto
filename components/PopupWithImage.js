824771dfed3f74f2beaa5d0485453a0bc9772bad
Sergey Dedikov <serded59@yandex.ru>
2021-10-09 19:09:40 +0500

    feat: creat class PopupImage

---
+++ components/PopupWithImage.js
@@ -0,0 +1,20 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(data, popupSelector) {
+    super(popupSelector);
+    this._name = data.name;
+    this._link = data.link;
+    this._image = this.popupSelector.querySelector(".popup__card-image");
+    this._description = this.popupSelector.querySelector(
+      ".popup__card-description"
+    );
+  }
+
+  open() {
+    this._image.src = this._link;
+    this._image.alt = "На фотографии: " + this._name;
+    this._description.textContent = this._name;
+    super.open();
+  }
+}
