9e042a609223f60d5258cb9e28ea4b9c4fcd09ec
Sergey Dedikov <serded59@yandex.ru>
2021-10-10 16:03:20 +0500

    fix: correct class popupimage

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,20 +1,16 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
-  constructor(data, popupSelector) {
+  constructor(popupSelector) {
     super(popupSelector);
-    this._name = data.name;
-    this._link = data.link;
     this._image = this._popup.querySelector(".popup__card-image");
-    this._description = this._popup.querySelector(
-      ".popup__card-description"
-    );
+    this._description = this._popup.querySelector(".popup__card-description");
   }
 
-  open() {
-    this._image.src = this._link;
-    this._image.alt = "На фотографии: " + this._name;
-    this._description.textContent = this._name;
+  open({ text, link }) {
+    this._image.src = link;
+    this._image.alt = "На фотографии: " + text;
+    this._description.textContent = text;
     super.open();
   }
 }
