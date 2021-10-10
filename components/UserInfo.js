d4ca8f7453519aae036557cf4c80929fb0c4daa0
Sergey Dedikov <serded59@yandex.ru>
2021-10-10 15:42:50 +0500

    feat: creat class UserInfo

---
+++ components/UserInfo.js
@@ -0,0 +1,18 @@
+export default class UserInfo {
+  constructor({ nameProfile, jobProfile }) {
+    this._name = nameProfile;
+    this._job = jobProfile;
+  }
+
+  getUserInfo() {
+    const userInfo = {};
+    userInfo.name = this._name.textContent;
+    userInfo.job = this._job.textContent;
+    return userInfo;
+  }
+
+  setUserInfo({ name, job }) {
+    this._name.textContent = name;
+    this._job.textContent = job;
+  }
+}
