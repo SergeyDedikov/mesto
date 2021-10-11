9230802baa967eb9ba0b360a1df75107ff4f5c7d
Sergey Dedikov <serded59@yandex.ru>
2021-10-11 19:51:59 +0500

    refactor: delete not used

--- utils/constants.js
+++ utils/constants.js
@@ -37,11 +37,6 @@ const popupCard = document.querySelector(".popup_type_card");
 const popupEditProfile = document.querySelector(".popup_type_edit-profile");
 const buttonEditProfile = document.querySelector(".profile__button_type_edit");
 const formEditProfile = document.querySelector(".popup__form_edit-profile");
-const buttonCloseEditProfile = document.querySelector(
-  ".popup__close_edit-profile"
-);
-const nameInput = formEditProfile.querySelector(".popup__input_value_name");
-const jobInput = formEditProfile.querySelector(".popup__input_value_job");
 const nameProfile = document.querySelector(".profile__name");
 const jobProfile = document.querySelector(".profile__job");
 
@@ -74,9 +69,6 @@ export {
   popupEditProfile,
   buttonEditProfile,
   formEditProfile,
-  buttonCloseEditProfile,
-  nameInput,
-  jobInput,
   nameProfile,
   jobProfile,
   popupAddPlace,
