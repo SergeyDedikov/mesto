a14effb700085c5371bcfd2c66f957d165f5f288
Sergey Dedikov <serded59@yandex.ru>
2021-10-11 19:16:52 +0500

    refactor: class PopupForm

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -16,17 +16,17 @@ export default class PopupWithForm extends Popup {
   }
 
   setEventListeners() {
+    super.setEventListeners();
     this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
 
       this._handleFormSubmit(this._getInputValues());
       this.close();
     });
-    super.setEventListeners();
   }
 
   close() {
-    this._form.reset();
     super.close();
+    this._form.reset();
   }
 }
