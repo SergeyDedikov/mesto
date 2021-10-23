b1748cc031f37769b94aee99b535fbb8ea56f5e1
Sergey Dedikov <serded59@yandex.ru>
2021-10-23 16:47:22 +0500

    feat: create class API

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,14 +3,16 @@ export default class Api {
     this._apiUrl = config.apiUrl;
     this._cohortId = config.cohortId;
     this._tokenId = config.tokenId;
+    this._headers = {
+      authorization: this._tokenId,
+      "Content-Type": "application/json",
+    };
   }
 
   getInitialCards() {
     return fetch(`${this._apiUrl}/v1/${this._cohortId}/cards`, {
       method: "GET",
-      headers: {
-        authorization: this._tokenId,
-      },
+      headers: this._headers,
     })
       .then((res) => {
         if (res.ok) {
@@ -27,6 +29,23 @@ export default class Api {
       });
   }
 
-
-
+  getUserInfo() {
+    return fetch(`${this._apiUrl}/v1/${this._cohortId}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return Promise.reject(`Ошибка: ${res.status}`);
+        }
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
